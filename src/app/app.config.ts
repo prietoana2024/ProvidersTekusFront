584e4148b7391c064faacd4dd40da5139016f4b2
anacprieto <aprieto@e-city.co>
2025-11-05 13:34:41 -0500

    token

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,10 +14,11 @@ export const appConfig: ApplicationConfig = {
 */
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http'; 
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http'; 
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { authInterceptorFn } from './Interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -25,6 +26,9 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch())
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([authInterceptorFn])
+    )
   ]
 };
